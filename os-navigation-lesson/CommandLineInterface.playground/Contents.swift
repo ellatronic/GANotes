2e6d16a4d31a3b226ae80af9baba98582c1452d2
ellatronic <ella@ellatronic.com>
2017-05-21 08:18:57 -0700

    Add OS Navigation lesson

---
+++ os-navigation-lesson/CommandLineInterface.playground/Contents.swift
@@ -0,0 +1,106 @@
+/*:
+ # ![GA icon](GAIcon.png) C
+ 
+ Week I DAY II
+ 
+ ## The topics we will cover are:
+ 
+ - Paths
+ - OS File System
+ - Command Line Interface
+ - Commands
+ 
+ If we have time, we will discuss shell scripts and a script that deletes derived data to help clear up space.
+ 
+ ## By the end of this lesson, we should be able to (i.e., Learning Objectives):
+ * Summarize a basic filesystem structure, including absolute and relative paths
+ * Identify the Command Line Interface (i.e., Terminal)
+ * Use the most common commands to navigate and modify files / directories via the Terminal window (`man`, `cd`, `ls`, `pwd`, `mkdir`, `rm -rf`, `mv`, `cp`, `touch`)
+ * Differentiate between navigating the file system using the CLI vs. the GUI
+ 
+ ## Connection to long term goal:
+ Terminal navigation provides low-level access to a system that developers need.
+ 
+ ## Student prework before we open!
+ * Xcode
+ 
+ */
+
+// write code here:
+
+/*:
+ 
+## Opening
+
+What is a path? What are the two most important paths?
+
+ * Absolute and relative paths. An absolute or full path points to the same location in a file system, regardless of the current working directory. To do that, it must include the root directory. By contrast, a relative path starts from some given working directory, avoiding the need to provide the full absolute path.
+ 
+ ### absolute path
+ `/Users/applocalization/Library/Developer/Xcode/DerivedData`
+ 
+ ### relative path
+ `Xcode/DerivedData`
+ 
+ */
+// write code here:
+
+/*:
+
+ ## What is the filesystem in Mac?
+ * UNIX 
+ 
+ */
+// write code here:
+
+/*:
+ 
+ 
+ ## Command Line Interface
+ * A CLI (command line interface) is a user interface to a computer's operating system or an application in which the user responds to a visual prompt by typing in a command on a specified line, receives a response back from the system, and then enters another command, and so forth
+ 
+ ### Syntax of a command
+ * **command** _option(s)_ _filename(s)_
+ 
+ ![command](command.png)
+ 
+ * A UNIX command may or may not have arguments. An argument can be an option or a filename.
+ * UNIX command lines can be simple, one-word entries like the date command. They can also be more complex: you may need to type more than the command name.
+ 
+ */
+
+// write code here:
+
+/*:
+ 
+ 
+ ## Closing
+ 
+ We covered file paths, the terminal, the command line interface, & commands. What do you think?
+ 
+ ### Class Discussion
+ 
+ Earlier we said that by the end of this lesson, we should be able to:
+ * Summarize a basic filesystem structure, including absolute and relative paths
+ * Identify the Command Line Interface (i.e., Terminal)
+ * Use the most common commands to navigate and modify files / directories via the Terminal window (`man`, `cd`, `ls`, `pwd`, `mkdir`, `rm -rf`, `mv`, `cp`, `touch`)
+ * Differentiate between navigating the file system using the CLI vs. the GUI
+ 
+ 
+ 
+ ### Takeaways
+ 
+ - What is a command? What is a Graphical User Interface?
+ 
+ ### Afterthoughts
+- If we have time, we will discuss shell scripts and a script that deletes derived data to help clear up space.
+ 
+ For more information on
+ 
+ - [The Linux Command Line](https://www.nostarch.com/tlcl)
+ - [Learn the Command Line](https://www.codecademy.com/learn/learn-the-command-line)
+ - [Introduction to Linux](https://www.edx.org/course/introduction-linux-linuxfoundationx-lfs101x-0)
+ 
+ */
+
+
